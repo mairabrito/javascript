0e7e85b80f992f2483e68b8d556f29ac3d2eea1c
Maíra Brito <104110095+mairabrito@users.noreply.github.com>
2022-04-20 22:26:34 -0300

    Create 15-usandoVar1.js

---
+++ fundamentos/15-usandoVar1.js
@@ -0,0 +1,18 @@
+{
+    {
+        {
+            {
+                var sera = 'Será???'
+                console.log(sera) // Vai imprimir
+            }
+        }
+    }
+}
+console.log(sera) // Tbm vai imprimir
+
+function teste(){
+    var local = 123;
+    console.log(local); // Vai imprimir
+}
+teste()
+console.log(local) // Não vai imprimir
