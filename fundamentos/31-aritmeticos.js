1d9ef06dac2c1a112edff47f19652f3177dfdf11
Maíra Brito <104110095+mairabrito@users.noreply.github.com>
2022-04-20 22:27:24 -0300

    Create 31-aritmeticos.js

---
+++ fundamentos/31-aritmeticos.js
@@ -0,0 +1,9 @@
+const [a,b,c,d] = [3,1,5,9]
+
+const soma = a+b+c+d
+const subtracao = d-a
+const mult = a*b
+const div = d/a
+const resto = d%2 //modulo
+
+console.log(soma, subtracao, mult, div, resto)
