a19f5c7f72fe1561d1d2bb99a66fb0ac62cb5be5
Maíra Brito <104110095+mairabrito@users.noreply.github.com>
2022-04-20 22:25:37 -0300

    Create 3-tipagemFraca.js

---
+++ fundamentos/3-tipagemFraca.js
@@ -0,0 +1,22 @@
+/*
+Tipagem dinâmica é uma característica de determinadas linguagens de programação,
+que não exigem declarações de tipos de dados, pois são capazes de escolher que 
+tipo utilizar dinamicamente para cada variável, podendo alterá-lo durante a 
+compilação ou a execução do programa.
+*/
+
+//TIPAGEM FRACA
+
+let qualquer = 'Legal';
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.1516
+console.log(qualquer);
+console.log(typeof qualquer);
+
+
+//Evitar nome genéricos e siglas
+let valor ='';
+let numero = 1;
+let pqp = false; //Produto Químico Perigoso.. kkk
