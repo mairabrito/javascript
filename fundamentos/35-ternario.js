24468d054c6eee5356b694402593740da0f81330
Maíra Brito <104110095+mairabrito@users.noreply.github.com>
2022-04-20 22:27:39 -0300

    Create 35-ternario.js

---
+++ fundamentos/35-ternario.js
@@ -0,0 +1,4 @@
+const resultado = nota => nota>=7 ? 'Aprovado':'Reprovado' //Se a expresso for verdadeira, retorna Aprovado, se não, retorna Reprovado
+
+console.log(resultado(7.1))
+console.log(resultado(6.9))
