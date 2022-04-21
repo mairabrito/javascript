54fb6611533404b858b0e7b94200d4f0f84a671c
Maíra Brito <104110095+mairabrito@users.noreply.github.com>
2022-04-20 22:25:48 -0300

    Create 5-math.js

---
+++ fundamentos/5-math.js
@@ -0,0 +1,7 @@
+//ÁREA DE UM CÍRCULO PI*Raio²
+
+const raio = 12.5;
+const area = Math.PI * Math.pow(raio,2);
+
+console.log(area.toFixed(2)); //toFixed(2) = duas casas decimais
+console.log(typeof Math); // Object
