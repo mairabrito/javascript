9feb764c2b39794402c078b427701d1c2ea75eb8
Maíra Brito <104110095+mairabrito@users.noreply.github.com>
2022-04-20 22:29:22 -0300

    Create ArrowFunction3.js

---
+++ funcao/ArrowFunction3.js
@@ -0,0 +1,19 @@
+let comparaComThis = function(param) {
+    console.log(this === param)
+}
+
+comparaComThis(global)
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj)
+comparaComThis(global)
+comparaComThis(obj)
+
+let comparaComThisArrow = param => console.log(this === param)
+
+comparaComThisArrow(global)
+comparaComThisArrow(module.exports)
+
+comparaComThisArrow = comparaComThisArrow.bind(obj)
+comparaComThisArrow(obj) //Arrow eh mais forte que o bind, não muda o contexto
+comparaComThisArrow(module.exports)
